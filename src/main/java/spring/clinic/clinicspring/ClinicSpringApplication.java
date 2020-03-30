c9f874ace0c7d7ce612a69e1b4caddc6dfaa25f2
D Paul Childs <clausewitzian@gmail.com>
2020-03-29 20:56:21 -0400

    init

---
+++ src/main/java/spring/clinic/clinicspring/ClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package spring.clinic.clinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicSpringApplication.class, args);
+    }
+
+}

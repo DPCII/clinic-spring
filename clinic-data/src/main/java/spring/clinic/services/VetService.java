41f140918240311fdb8f98c6c4133de5b50fd64a
D Paul Childs <clausewitzian@gmail.com>
2020-03-31 11:07:32 -0400

    Refactored services to common interface. Closed #11.

--- clinic-data/src/main/java/spring/clinic/services/VetService.java
+++ clinic-data/src/main/java/spring/clinic/services/VetService.java
@@ -2,15 +2,6 @@ package spring.clinic.services;
 
 import spring.clinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
+public interface VetService extends CrudService<Vet, Long> {
     Vet findByLastName(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }

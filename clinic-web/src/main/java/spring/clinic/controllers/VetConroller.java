b179aa2edd6dffe583edceee2e83847be3dde602
D Paul Childs <clausewitzian@gmail.com>
2020-04-02 08:50:49 -0400

    Added vet list. Closes #15.

--- clinic-web/src/main/java/spring/clinic/controllers/VetConroller.java
+++ clinic-web/src/main/java/spring/clinic/controllers/VetConroller.java
@@ -1,13 +1,23 @@
 package spring.clinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.clinic.services.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetConroller {
 
-    @RequestMapping("/vets")
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetConroller(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }

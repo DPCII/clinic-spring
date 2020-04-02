bfe9054eb2da63f6a24765a354c25f20cedec069
D Paul Childs <clausewitzian@gmail.com>
2020-04-02 16:19:33 -0400

    Added PetType to bootstrap data and made Pet Services impl. Closes #25.

---
+++ clinic-data/src/main/java/spring/clinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package spring.clinic.services;
+
+import spring.clinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ clinic-data/src/main/java/spring/clinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package spring.clinic.services.map;
+
+import org.springframework.stereotype.Service;
+import spring.clinic.models.PetType;
+import spring.clinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}

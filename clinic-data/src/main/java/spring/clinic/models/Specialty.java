1f2655e554c2d918530433d3b6c9883b0cf0de37
D Paul Childs <clausewitzian@gmail.com>
2020-04-02 15:43:18 -0400

    Associated vet to specialty. Closes #23.

---
+++ clinic-data/src/main/java/spring/clinic/models/Specialty.java
@@ -0,0 +1,14 @@
+package spring.clinic.models;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
